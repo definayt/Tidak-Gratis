2bccd38376662748ee6e1d90acc2eb06c326f094
definayt <definayt@users.noreply.github.com>
2020-06-18 00:16:29 +0700

    db2

--- TidakGratisAdmin/shop/connect.php
+++ TidakGratisAdmin/shop/connect.php
@@ -1,7 +1,7 @@
 <?php 
 	$db_host ="localhost"; 
 	$db_username ="root"; 
-	$db_password =""; 
+	$db_password ="24060117120039"; 
 	$db_database ="shop"; 
 	$con = mysqli_connect($db_host, $db_username, $db_password,$db_database);
 	  if (mysqli_connect_errno()){
