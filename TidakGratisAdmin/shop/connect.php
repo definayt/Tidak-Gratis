0d1aad7b98e0ce4d1c104e1dc2b64a5ed4b5a79f
definayt <definayt@users.noreply.github.com>
2020-06-19 08:56:58 +0700

    db

--- TidakGratisAdmin/shop/connect.php
+++ TidakGratisAdmin/shop/connect.php
@@ -1,10 +1,10 @@
 <?php 
-	$db_host ="132.145.161.151"; 
+	$db_host ="localhost"; 
 	$db_username ="root"; 
 	$db_password ="24060117120039"; 
 	$db_database ="shop"; 
 	$con = mysqli_connect($db_host, $db_username, $db_password,$db_database);
 	  if (mysqli_connect_errno()){
-	    die ("Could not connect to the database: <br />". mysqli_connect_error(). mysqli_connect_errno());
+	    die ("Could not connect to the database: <br />". mysqli_connect_error());
 	  }
 ?>
