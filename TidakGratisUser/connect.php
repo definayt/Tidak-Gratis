de98be5328e9e9591abc92498341824ba3974df8
definayt <definayt@users.noreply.github.com>
2020-06-22 09:05:52 +0700

    Update connect.php

--- TidakGratisUser/connect.php
+++ TidakGratisUser/connect.php
@@ -1,6 +1,6 @@
 <?php 
 	$db_host ="132.145.161.151"; 
-	$db_username ="2406011720039"; 
+	$db_username ="24060117120039"; 
 	$db_password ="password"; 
 	$db_database ="shop"; 
 	$con = mysqli_connect($db_host, $db_username, $db_password,$db_database);
