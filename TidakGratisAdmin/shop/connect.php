c2cacf310b0ebcc3867162a796bed28e1d60c47d
definayt <definayt@users.noreply.github.com>
2020-06-24 20:14:46 +0700

    test

--- TidakGratisAdmin/shop/connect.php
+++ TidakGratisAdmin/shop/connect.php
@@ -1,10 +1,10 @@
 <?php 
-	// $db_host ="129.213.56.240"; 
-	$db_host ="localhost"; 
-	// $db_username ="24060117120039"; 
-	$db_username ="root";
-	// $db_password ="password"; 
-	$db_password ="Iadhisti-2002";
+	$db_host ="129.213.56.240"; 
+	// $db_host ="localhost"; 
+	$db_username ="24060117120039"; 
+	// $db_username ="root";
+	$db_password ="password"; 
+	// $db_password ="Iadhisti-2002";
 	$db_database ="shop"; 
 	$con = mysqli_connect($db_host, $db_username, $db_password,$db_database);
 	  if (mysqli_connect_errno()){
